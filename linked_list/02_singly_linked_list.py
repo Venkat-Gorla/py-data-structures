29a45f4c49ef0ad26365de3efd2e000693c12b04
Venkat Gorla <venkatagorla@gmail.com>
2025-01-14 15:34:00 +0530

    Singly linked list reverse between start and end indexes, test cases

--- linked_list/02_singly_linked_list.py
+++ linked_list/02_singly_linked_list.py
@@ -1,6 +1,6 @@
 
-# given a singly linked list (sll), reverse it between a pair of start and end indexes, inclusive
-# the list is 0 indexed and you can assume 'start' and 'end' indexes are valid
+# given a singly linked list (sll), reverse it between a pair of start and end indexes, inclusive.
+# the list is 0 indexed and you can assume 'start' and 'end' indexes are valid.
 # example:
 # 1->2->3->4->5 and reverse_between(1, 3) should yield
 # 1->4->3->2->5
@@ -39,8 +39,32 @@ class SinglyLinkedList:
             current = current.next
         print('None')
 
+    # solution function
     def reverse_between(self, start_index, end_index):
-        return
+        if self.head is None or start_index == end_index:
+            return
+
+        dummy_node = Node(0)
+        dummy_node.next = self.head
+
+        prefix = dummy_node
+        for _ in range(start_index):
+            prefix = prefix.next
+
+        previous = None
+        current = prefix.next
+        for _ in range(end_index - start_index + 1):
+            next_node = current.next
+            current.next = previous
+            previous = current
+            current = next_node
+
+        first_reversed_node = prefix.next
+        prefix.next = previous
+        first_reversed_node.next = current
+
+        if start_index == 0:
+            self.head = previous
 
     # useful for test cases
     def from_list(self, data):
@@ -58,9 +82,29 @@ class SinglyLinkedList:
 def test_list_creation():
     sll = SinglyLinkedList()
     sll.from_list([1, 2, 3, 4, 5])
-    sll.print_list()
     assert sll.to_list() == [1, 2, 3, 4, 5]
+    print("List creation test passed")
+
+def test_list_reverse_between():
+    def build_and_reverse_between(lst, start, end):
+        sll = SinglyLinkedList()
+        sll.from_list(lst)
+        sll.reverse_between(start, end)
+        return sll.to_list()
+
+    # Test cases
+    assert build_and_reverse_between([1, 2, 3, 4, 5], 1, 3) == [1, 4, 3, 2, 5]
+    assert build_and_reverse_between([1, 2, 3, 4, 5], 0, 4) == [5, 4, 3, 2, 1]
+    assert build_and_reverse_between([1, 2, 3, 4, 5], 0, 0) == [1, 2, 3, 4, 5]
+    assert build_and_reverse_between([1, 2, 3, 4, 5], 0, 2) == [3, 2, 1, 4, 5]
+    assert build_and_reverse_between([], 0, 0) == []
+    print("List reverse between tests passed")
 
 if __name__ == '__main__':
     test_list_creation()
+    test_list_reverse_between()
+
+# output
+# List creation test passed
+# List reverse between tests passed
 
