31fb5958839a3209990ff881fe5fac546ae79fa4
Venkat Gorla <venkatagorla@gmail.com>
2025-01-24 12:04:26 +0530

    Hash table practice: given a list of strings, find and group anagrams.

---
+++ hash_table/01_group_anagrams.py
@@ -0,0 +1,29 @@
+
+# given a list of strings, find and group anagrams.
+# you should return list<list<string>> where each inner list represents a group of anagrams.
+# for example: "abc" and "bca" are anagrams and should be grouped inside the same inner list.
+
+def find_anagrams(input_list):
+    anagram_groups = {}
+
+    for word in input_list:
+        sorted_word = ''.join(sorted(word))
+        anagram_groups.setdefault(sorted_word, []).append(word)
+
+    return list(anagram_groups.values())
+
+def test_find_anagrams():
+    input_list = ["eat", "tea", "tan", "ate", "nat", "bat"]
+    assert find_anagrams(input_list) == [['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']]
+
+    input_list = ["abc", "bca", "cab"]
+    assert find_anagrams(input_list) == [['abc', 'bca', 'cab']]
+
+    print('All anagram tests passed')
+
+if __name__ == '__main__':
+    test_find_anagrams()
+
+# output
+# All anagram tests passed
+
